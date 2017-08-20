ab99991cad1b45251b0c59c3686ed8dbcef48925
mikkokut <mikko93@gmail.com>
2017-08-20 12:45:19 +0300

    use current law, parsing fixes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 var Crawler = require('crawler');
 var Algoliasearch = require('algoliasearch');
+var Cheerio = require('cheerio');
 
 var algoliaClient = Algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_SECRET);
 var sectionIndex = algoliaClient.initIndex('sections');
 var lawIndex = algoliaClient.initIndex('laws');
 
 var fromYear = 1700;
+var toYear = (new Date()).getFullYear();
 
 var FINLEX_BASE = 'http://www.finlex.fi';
 
@@ -15,11 +17,11 @@ var FINLEX_BASE = 'http://www.finlex.fi';
 var lawCount = 0;
 var sectionCount = 0;
 
-var parseYearCollection = function($, year) {
+
+var parseYearCollection = function($, year, done) {
 	$('.doc').each(function(index, elem) {
 		var url = FINLEX_BASE + $(this).find('a').attr('href');
-
-		c.queue({
+		queue({
 			uri: url,
 			type: 'law',
 			law: {
@@ -29,9 +31,12 @@ var parseYearCollection = function($, year) {
 			}
 		});
 	});
+
+	done();
 };
 
-var parseLaw = function($, law) {
+
+var parseLaw = function($, law, done) {
 	var $document = $('#document');
 
 	var l = {
@@ -44,8 +49,7 @@ var parseLaw = function($, law) {
 	};
 
 	lawCount++;
-	//lawIndex.addObject(l);
-	console.log('Laws: ' + lawCount);
+	lawIndex.addObject(l);
 
 	// Find all sections
 	var $lastSectionHeader = null;
@@ -54,59 +58,103 @@ var parseLaw = function($, law) {
 			if ($lastSectionHeader) {
 				var title = $lastSectionHeader.text().trim();
 				if (title) {
+
+					var content = [];
+					$(this).prevUntil($lastSectionHeader, 'p.py').each(function() {
+
+						// Find links, if the link starts with the "#" we want to append the 
+						// current page url so it works for use too.
+						$(this).find('a').each(function() {
+							var href = $(this).attr('href') || '';
+							if (href.substr(0,1) === '#') {
+								$(this).attr('href', law.url + href);
+							}
+
+							$(this).attr('target', '_blank');
+						});
+
+						content.unshift('<p>' + $(this).html() + '</p>');
+					});
+
 					var section = {
 						title: title,
-						content: $(this).prevUntil($lastSectionHeader).html(),
+						content: content.join(''),
 						url: law.url + '#' + $lastSectionHeader.attr('id').trim(),
 						law: l,
 						objectID: law.name + '§' + $lastSectionHeader.attr('id').trim()
 					};
 
 					sectionCount++;
-					//sectionIndex.addObject(section);
-					console.log('Sections: ' + sectionCount);
+					sectionIndex.addObject(section);
 				}
 			}
 
 			$lastSectionHeader = $(this);
 		}
 	});
+
+	console.log('Laws: ' + lawCount);
+	console.log('Sections: ' + sectionCount);
+	console.log('Left: ' + c.queueSize);
+
+	done();
 };
 
 var c = new Crawler({
-    maxConnections : 50,
+    maxConnections : 5,
+    jQuery: false,
     callback : function (error, res, done) {
-        if(error) console.log('Failed. ' + res.type + ' ' + error);
+        if(error) {
+        	console.log('Failed. ' + res.type + ' ' + error);
+        	done();
+        }
         else {
-            var $ = res.$;
+            var $ = Cheerio.load(res.body, {decodeEntities: false, normalizeWhitespace: true});
+
         	if (res.options.type === 'year-collection') {
         		$('.result-pages a').each(function(index, elem) {
         			var url = FINLEX_BASE + $(this).attr('href');
-        			c.queue({
+        			queue({
         				uri: url,
         				type: 'year-collection-sub'
         			});
         		});
 
-        		parseYearCollection($, res.options.year);
+        		parseYearCollection($, res.options.year, done);
         	}
         	else if (res.options.type === 'year-collection-sub') {
-				parseYearCollection($, res.options.year);
+				parseYearCollection($, res.options.year, done);
         	}
         	else if (res.options.type === 'law') {
-        		parseLaw($, res.options.law);
+        		parseLaw($, res.options.law, done);
+        	}
+        	else {
+        		done();
         	}
         }
-
-        done();
     }
 });
 
+c.on('drain', function() {
+    console.log('QUEUE EMPTY');
+});
+
+var urls = [];
+var queue = function(options) {
+	if (urls.includes(options.uri)) {}
+	else {
+		console.log(options.uri);
+		urls.push(options.uri);
+		c.queue(options);
+	}
+};
+
 
 // Fetch all years
-for (var year = fromYear; year <= 2017; year++) {
-	c.queue({
-		uri: FINLEX_BASE + '/fi/laki/alkup/'+year+'/',
+for (var year = fromYear; year <= toYear; year++) {
+	var url = FINLEX_BASE + '/fi/laki/ajantasa/'+year+'/';
+	queue({
+		uri: url,
 		type: 'year-collection',
 		year: year
 	});
